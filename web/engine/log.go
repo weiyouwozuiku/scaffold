4189f1bb32794bdb254dd84d02e608ba304c10a8
buer <19681022+weiyouwozuiku@users.noreply.github.com>
2023-11-13 23:42:24 +0800

    save

--- web/engine/log.go
+++ web/engine/log.go
@@ -2,11 +2,25 @@ package engine
 
 import (
 	"context"
+	"fmt"
 	"log/slog"
+	"os"
 )
 
-type Logger struct {
-	slog.Logger
+var Logger logger
+
+type logger struct {
+	log *slog.Logger
 }
 
-func (l *Logger) Log(ctx context.Context)
+func NewLogger() error {
+	Logger.log = slog.New(slog.NewJSONHandler(os.Stdout, nil))
+	return nil
+}
+
+type LogFuncWithCtx func(ctx context.Context, prefix string, format string, args ...any)
+
+func (l *logger) Errorf(ctx context.Context, prefix string, format string, args ...any) {
+	l.log.Log(ctx, slog.LevelError, prefix, args...)
+}
+func (l *logger) Infof(ctx context.Context,prefix )
--- web/engine/router_test.go
+++ web/engine/router_test.go
@@ -8,6 +8,7 @@ import (
 )
 
 func TestSlog(t *testing.T) {
-	jsonLoger := slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{})).With()
-	jsonLoger.Log(context.Background(), slog.LevelInfo, "hhh||error=%v", "111", "2")
+	jsonLoger := slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{AddSource: true}))
+	jsonLoger.With("1q1q1", "w2w2w2").Log(context.Background(), slog.LevelInfo, "hhh||error=%v", "111", "2")
+	jsonLoger.Log(context.Background(), slog.LevelInfo, "21212")
 }
