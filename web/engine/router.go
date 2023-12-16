0f982c2fe0dd252869fb319dd5c64f7706ea2f2b
buer <19681022+weiyouwozuiku@users.noreply.github.com>
2023-12-17 00:12:17 +0800

    save

--- web/engine/router.go
+++ web/engine/router.go
@@ -1,5 +1,10 @@
 package engine
 
+import (
+	"net/http"
+	"strings"
+)
+
 type router struct {
 	roots    map[string]*node
 	handlers map[string]HandlerFunc
@@ -11,6 +16,58 @@ func newRouter() *router {
 		handlers: make(map[string]HandlerFunc),
 	}
 }
+<<<<<<< Updated upstream
 func (r *router) addRouter(method, pattern string, hanlder HandlerFunc) {
 
 }
+=======
+
+func parsePattern(pattern string) []string {
+	info := strings.Split(pattern, "/")
+	parts := make([]string, 0)
+	for _, item := range info {
+		if item != "" {
+			parts = append(parts, item)
+			if item[0] == '*' {
+				break
+			}
+		}
+	}
+	return parts
+}
+
+func (r *router) addRoute(method, pattern string, handler HandlerFunc) {
+	parts := parsePattern(pattern)
+	key := method + "-" + pattern
+	_, ok := r.roots[method]
+	if !ok {
+		r.roots[method] = &node{}
+	}
+	r.roots[method].insert(pattern, parts, 0)
+	r.handlers[key] = handler
+}
+
+func (r *router) getRoute(method, path string) (*node, map[string]string) {
+	root, ok := r.roots[method]
+	if !ok {
+		return nil, nil
+	}
+	searchParts := parsePattern(path)
+	n := root.search(searchParts, 0)
+	if n != nil {
+
+	}
+
+}
+
+func (r *router) handler(c *Context) {
+	n, params := r.getRoute(c.Method, c.Path)
+	if n != nil {
+		c.Params = params
+		key := c.Method + "-" + n.pattern
+		r.handlers[key](c)
+	} else {
+		c.STRING(http.StatusNotFound, "404 NOT FOUND: %s\n", c.Path)
+	}
+}
+>>>>>>> Stashed changes
--- web/engine/trie.go
+++ web/engine/trie.go
@@ -1,8 +1,48 @@
 package engine
 
+import (
+	"fmt"
+	"strings"
+)
+
 type node struct {
 	pattern  string  // 待匹配路由，例如 /p/:lang
 	part     string  // 路由中的一部分，例如 :lang
 	children []*node // 子节点，例如 [doc, tutorial, intro]
 	isWild   bool    // 是否精确匹配，part 含有 : 或 * 时为true
+}
+
+func (n *node) String() string {
+	return fmt.Sprintf("node{pattern=%s, part=%s, isWild=%t}", n.pattern, n.part, n.isWild)
+}
+func (n *node) matchChild(part string) *node {
+	for _, it := range n.children {
+		if it.part == part || it.isWild {
+			return it
+		}
+	}
+	return nil
+}
+func (n *node) insert(pattern string, parts []string, height int) {
+	// 退出递归条件
+	if len(parts) == height {
+		n.pattern = pattern
+		return
+	}
+	// url中的/分割组中的一个
+	part := parts[height]
+	child := n.matchChild(part)
+	if child == nil {
+		child = &node{part: part, isWild: part[0] == ':' || part[0] == '*'}
+		n.children = append(n.children, child)
+	}
+	child.insert(pattern, parts, height+1)
+}
+func (n *node) search(parts []string, height int) *node {
+	if len(parts) == height || strings.HasPrefix(n.part, "*") {
+		if n.pattern == "" {
+			return nil
+		}
+		return n
+	}
 }
